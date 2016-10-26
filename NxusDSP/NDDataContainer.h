370f74fb65b57160b86ed76e679aff239994a07f
SDK Development <sdkdevelopment@mpiremedia.com.au>
2016-10-26 13:53:01 +0200

    Initial import.

---
+++ NxusDSP/NDDataContainer.h
@@ -0,0 +1,34 @@
+//
+//  NDDataContainer.h
+//  NxusDSP
+//
+//  Copyright © 2016. TechMpire ltd. All rights reserved.
+//
+
+#import "TrackingItem.h"
+
+@interface NDDataContainer : NSObject {}
+
+@property (nonatomic, strong) NSString *sdkPlatform;
+
++ (NSString *) pullStringValue: (NSString *) key;
++ (void) storeStringValue: (NSString *) key value:(NSString *) value;
+
++ (int) pullIntValue: (NSString *) key;
++ (void) storeIntValue: (NSString *) key value:(int) value;
+
++ (double) pullDoubleValue: (NSString *) key;
++ (void) storeDoubleValue: (NSString *) key value:(double) value;
+
++ (NSMutableArray *) pullAllTrackingEvents;
++ (TrackingItem *) pullTrackingEvent;
++ (void) storeTrackingEvent:(TrackingItem *)trackingEvent;
++ (void) clearTrackingEvent:(TrackingItem *)trackingEvent;
+
++ (NSString *) pullAdvertisingIdentifier;
++ (void) storeAdvertisingIdentifier:(NSString *)advertisingIdentifier;
+
++ (void) setSdkPlatform:(NSString *)platform;
++ (NSString *) getSdkPlatform;
+
+@end
---
+++ Podfile
@@ -0,0 +1,12 @@
+project 'NxusDSP.xcodeproj'
+
+# Uncomment this line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'NxusDSP' do
+  # Uncomment this line if you're using Swift or would like to use dynamic frameworks
+  # use_frameworks!
+
+  # Pods for NxusDSP
+
+end
